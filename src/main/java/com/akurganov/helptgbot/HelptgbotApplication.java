59ca38c09d14af119a6cab17b07a6a8c74a23fa2
AKurganov <akurganov@croc.ru>
2021-03-21 13:50:47 +0300

    init java project

---
+++ src/main/java/com/akurganov/helptgbot/HelptgbotApplication.java
@@ -0,0 +1,13 @@
+package com.akurganov.helptgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelptgbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelptgbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akurganov/helptgbot/HelptgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akurganov.helptgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelptgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
