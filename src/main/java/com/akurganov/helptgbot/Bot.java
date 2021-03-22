e771c241ffa729dcd644623c1dfaf280b568680b
AKurganov <akurganov@croc.ru>
2021-03-22 18:28:35 +0300

    add tg dependency

---
+++ src/main/java/com/akurganov/helptgbot/Bot.java
@@ -0,0 +1,149 @@
+package com.akurganov.helptgbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Value("${telegram.bot.username}")
+    private String username;
+
+    private static final Logger LOG = LoggerFactory.getLogger(Bot.class);
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+
+        LOG.info("Получено сообщение {}", message);
+
+        sendMsg(update.getMessage().getChatId().toString(), message);
+
+    }
+
+    /*@Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()) {
+            ThreadClass thread = new ThreadClass(update.getMessage());
+        } else  if(update.hasCallbackQuery()) {
+            AnswerCallbackThread answerThread = new AnswerCallbackThread(update.getCallbackQuery());
+        }
+    }*/
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+
+        setButtons(sendMessage);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            LOG.error("Exception: {}", e.toString());
+        }
+    }
+
+    /**
+     * Пример создания кнопок под строкой ввода.
+     * Кнопки привязываются к сообщению.
+     *
+     * @param sendMessage отправляемое сообщение
+     */
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Привет"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add(new KeyboardButton("Помощь"));
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    /**
+     * Создание инлайн клавиатуры привязанной к конкретному сообщению.
+     */
+    private void setInline() {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+        //buttons1.add(new InlineKeyboardButton().setText("Кнопка").setCallbackData("17"));
+        buttons.add(buttons1);
+
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        markupKeyboard.setKeyboard(buttons);
+    }
+
+    /*public synchronized void answerCallbackQuery(String callbackId, String message) {
+        AnswerCallbackQuery answer = new AnswerCallbackQuery();
+        answer.setCallbackQueryId(callbackId);
+        answer.setText(message);
+        answer.setShowAlert(true);
+        try {
+            answerCallbackQuery(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
